80ab834c2a95ed761d6afd997c4dd9adab13d216
Jacqueline Quinteros <giselaquinteros783@gmail.com>
2023-08-14 08:31:20 -0600

    Cambios de las 8:30 --> Programacion de Entidades y DALs

--- JGQZ14082023.EntidadesDeNegocio/src/jgqz14082023/entidadesdenegocio/Tarea.java
+++ JGQZ14082023.EntidadesDeNegocio/src/jgqz14082023/entidadesdenegocio/Tarea.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package jgqz14082023.entidadesdenegocio;
 
-/**
- *
- * @author Gisela
- */
 public class Tarea {
+    private int id;
+    private String Titulo;
+    private String Descripcion;
     
+    private int top_aux;
+
+    public Tarea() {
+    }
+
+    public Tarea(int id, String Titulo, String Descripcion) {
+        this.id = id;
+        this.Titulo = Titulo;
+        this.Descripcion = Descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Titulo) {
+        this.Titulo = Titulo;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
 }
